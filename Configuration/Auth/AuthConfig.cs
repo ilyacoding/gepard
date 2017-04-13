bce885c631f414ee84c6235543c21d75255615cc
IlyaCoding <ilyacoding@yandex.com>
2017-04-14 02:14:52 +0300

    Created Auth Configuration classes

---
+++ Configuration/Auth/AuthConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gepard.Configuration.Auth
+{
+    public class AuthConfig
+    {
+        public string AuthType { get; set; }
+        public string AuthName { get; set; }
+    }
+}
